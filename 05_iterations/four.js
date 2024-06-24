666372c3192a9eb3d1bb8739fecd85754ad4c7ac
Saumitrakr <124288818+Saumitrakr@users.noreply.github.com>
2024-06-24 13:55:54 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,15 +5,19 @@ const myObject = {
     swift: "swift by apple"
 }
 
+// Iterates on keys of myObject
 for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+// here it will iterate on index of array as keys. Basically Arrays are object with keys as numbers 0,1,2,....
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
     //console.log(programming[key]);
 }
+
+// Maps are not iterable by for in loop
 
 // const map = new Map()
 // map.set('IN', "India")
